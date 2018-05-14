0eb239535d69909814d1f9ee095b01ac7e3321f5
AlexT <alexander.torres@radiancetech.com>
2018-05-14 15:01:15 -0500

    added a test when a user selects a execute sequence

--- pcp_alpha/Backend/pcp_app/tests.py
+++ pcp_alpha/Backend/pcp_app/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate
-from .views import get_command_list
+from .views import get_command_list, execute_sequence
 
 import pytest
 
@@ -16,5 +16,17 @@ def test_capability_data(rf):
     if user is not None:
         request = rf.get('action/get_capability_list/')
         response = get_command_list(request)
+        assert response.status_code == 200
+
+
+def test_execute_data(rf):
+    username = "pcpuser"
+    password = "pcpuser123"
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        request = rf.get('action/get_w3_data/')
+        response = execute_sequence(request)
         assert response.status_code == 200
 
