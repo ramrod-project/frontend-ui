f49168a687f761be1f3c43d8a2e6b46b0c76a1e5
AlexT <alexander.torres@radiancetech.com>
2018-05-21 11:03:25 -0500

    added test to test the controller that is in charge of displaying data to w4

--- pcp_alpha/Backend/pcp_app/tests.py
+++ pcp_alpha/Backend/pcp_app/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate
-from .views import get_commands_controller, execute_sequence_controller
+from .views import get_commands_controller, execute_sequence_controller, w4_output_controller
 
 import pytest
 
@@ -7,7 +7,14 @@ import pytest
 pytestmark = pytest.mark.django_db(transaction=True)
 
 
-def test_capability_data(rf):
+def test_display_capability_list_data(rf):
+    """
+    This test is testing to pull the command list and
+    display it to W2.
+    Task pcp-141
+    :param rf: request
+    :return: pass (status code of 200) or fail
+    """
     username = "pcpuser"
     password = "pcpuser123"
     user = authenticate(username=username, password=password)
@@ -18,7 +25,14 @@ def test_capability_data(rf):
         assert response.status_code == 200
 
 
-def test_execute_data(rf):
+def test_execute_w3_data(rf):
+    """
+    This test is testing data in w3 when the user clicks on
+    'Execute Sequence' button at the bottom right of w3.
+    Task pcp-142
+    :param rf: request
+    :return: pass (status code of 200) or fail
+    """
     username = "pcpuser"
     password = "pcpuser123"
 
@@ -27,5 +41,24 @@ def test_execute_data(rf):
     if user is not None:
         request = rf.get('action/get_w3_data/')
         response = execute_sequence_controller(request)
+        assert response.status_code == 200
+
+
+def test_display_w4_data(rf):
+    """
+    This test is testing to display data in w4.  The data
+    that displays depends on the status of the job.
+    Task pcp-140
+    :param rf: request
+    :return: pass (status code of 200) or fail
+    """
+    username = "pcpuser"
+    password = "pcpuser123"
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        request = rf.get('action/get_output_data/')
+        response = w4_output_controller(request)
         assert response.status_code == 200
 
