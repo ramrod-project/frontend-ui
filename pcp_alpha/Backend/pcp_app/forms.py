4aa79ea4cd09373cf4b2e29bde3321cf87386248
Dan Bauman <dan@bauman.space>
2018-07-10 12:10:14 -0500

    pcp-312 - client side validation

--- pcp_alpha/Backend/pcp_app/forms.py
+++ pcp_alpha/Backend/pcp_app/forms.py
@@ -6,7 +6,7 @@ class TargetForm(forms.Form):
     TargetForm class is the template to add a new target as a form.
     """
     plugin_name = forms.CharField(label='plugin_name', max_length=100)
-    location_num = forms.CharField(label='location_num', max_length=15)
+    location_num = forms.CharField(label='location_num', max_length=32)
     port_num = forms.CharField(label='port_num', max_length=4)
     optional_char = forms.CharField(label='optional', max_length=100, required=False)
 
