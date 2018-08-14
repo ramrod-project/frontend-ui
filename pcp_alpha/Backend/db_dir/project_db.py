64e5273293c8287db04ddc8697c57f323878a1e6
AlexT <alexander.torres@radiancetech.com>
2018-08-14 09:44:39 -0500

    pcp-424 scroll on w1 working but drag and drop not so much. #SadDay

--- pcp_alpha/Backend/db_dir/project_db.py
+++ pcp_alpha/Backend/db_dir/project_db.py
@@ -29,6 +29,24 @@ _TEST_TARGETS = [
         "Optional": "Document Here"
     },
     {
+        "PluginName": "Plugin2",
+        "Location": location_generated_num("172.16.5."),
+        "Port": "8002",
+        "Optional": "Document Here"
+    },
+    {
+        "PluginName": "Plugin3",
+        "Location": location_generated_num("172.16.5."),
+        "Port": "8002",
+        "Optional": "Document Here"
+    },
+    {
+        "PluginName": "Plugin4",
+        "Location": location_generated_num("172.16.5."),
+        "Port": "8002",
+        "Optional": "Document Here"
+    },
+    {
         "PluginName": "Plugin1",
         "Location": location_generated_num("172.16.5."),
         "Port": "8002",
