0f8e465c106d7e37676610685437c3cdfea1b213
AlexT <alexander.torres@radiancetech.com>
2018-08-27 13:23:31 -0500

    testing

--- pcp_alpha/Backend/db_dir/project_db.py
+++ pcp_alpha/Backend/db_dir/project_db.py
@@ -107,30 +107,30 @@ _TEST_COMMANDS = [
 ]
 
 plugins = [{
-    "id": "1-1-A",
-    "Name": "Plugin1",
-    "ServiceName": "Plugin1-a",
-    "ServiceID": "cheeto1",
-    "State": "Available",
-    "DesiredState": "",
-    "OS": "posix",
-    "Interface": "192.16.5.240",
-    "Environment": ["STAGE=DEV", "NORMAL=1"],
-    "ExternalPorts": ["9999/tcp"],
-    "InternalPorts": ["9999/tcp"]
-}, {
-    "id": "2-2-B",
-    "Name": "Plugin2",
-    "ServiceName": "Plugin2-a",
-    "ServiceID": "cheeto2",
-    "State": "Restarting",
-    "DesiredState": "",
-    "OS": "nt",
-    "Interface": "10.10.10.10",
-    "Environment": ["STAGE=DEV", "NORMAL=2"],
-    "ExternalPorts": ["4242/tcp"],
-    "InternalPorts": ["4242/tcp"]
-}, {
+#     "id": "1-1-A",
+#     "Name": "Plugin1",
+#     "ServiceName": "Plugin1-a",
+#     "ServiceID": "cheeto1",
+#     "State": "Available",
+#     "DesiredState": "",
+#     "OS": "posix",
+#     "Interface": "192.16.5.240",
+#     "Environment": ["STAGE=DEV", "NORMAL=1"],
+#     "ExternalPorts": ["9999/tcp"],
+#     "InternalPorts": ["9999/tcp"]
+# }, {
+#     "id": "2-2-B",
+#     "Name": "Plugin2",
+#     "ServiceName": "Plugin2-a",
+#     "ServiceID": "cheeto2",
+#     "State": "Restarting",
+#     "DesiredState": "",
+#     "OS": "nt",
+#     "Interface": "10.10.10.10",
+#     "Environment": ["STAGE=DEV", "NORMAL=2"],
+#     "ExternalPorts": ["4242/tcp"],
+#     "InternalPorts": ["4242/tcp"]
+# }, {
     "id": "3-3-C",
     "Name": "Plugin3",
     "ServiceName": "Plugin3-a",
