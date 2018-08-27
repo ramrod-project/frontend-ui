ca874fe2416e4acb076420deb84c22c5901f503d
AlexT <alexander.torres@radiancetech.com>
2018-08-27 13:25:16 -0500

    code climate is sensitive

--- pcp_alpha/Backend/db_dir/project_db.py
+++ pcp_alpha/Backend/db_dir/project_db.py
@@ -6,6 +6,7 @@ and validating information in the Brain.
 import sys
 from brain import connect, r as rtdb
 from brain import check_dev_env, check_prod_env
+from . import plugins
 
 from .custom_data import (
     location_generated_num,
@@ -105,44 +106,6 @@ _TEST_COMMANDS = [
         "OptionalInputs": []
     },
 ]
-
-plugins = [{
-#     "id": "1-1-A",
-#     "Name": "Plugin1",
-#     "ServiceName": "Plugin1-a",
-#     "ServiceID": "cheeto1",
-#     "State": "Available",
-#     "DesiredState": "",
-#     "OS": "posix",
-#     "Interface": "192.16.5.240",
-#     "Environment": ["STAGE=DEV", "NORMAL=1"],
-#     "ExternalPorts": ["9999/tcp"],
-#     "InternalPorts": ["9999/tcp"]
-# }, {
-#     "id": "2-2-B",
-#     "Name": "Plugin2",
-#     "ServiceName": "Plugin2-a",
-#     "ServiceID": "cheeto2",
-#     "State": "Restarting",
-#     "DesiredState": "",
-#     "OS": "nt",
-#     "Interface": "10.10.10.10",
-#     "Environment": ["STAGE=DEV", "NORMAL=2"],
-#     "ExternalPorts": ["4242/tcp"],
-#     "InternalPorts": ["4242/tcp"]
-# }, {
-    "id": "3-3-C",
-    "Name": "Plugin3",
-    "ServiceName": "Plugin3-a",
-    "ServiceID": "cheeto3",
-    "State": "Stopped",
-    "DesiredState": "",
-    "OS": "all",
-    "Interface": "192.16.5.240",
-    "Environment": ["STAGE=DEV", "NORMAL=3"],
-    "ExternalPorts": ["4243/udp"],
-    "InternalPorts": ["4243/udp"]
-}]
 
 
 TEST_PORT_DATA = {
