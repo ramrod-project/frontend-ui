ea8933a52c27b3ce2b87904d1d23b291ed270327
AlexT <alexander.torres@radiancetech.com>
2018-11-16 12:54:01 -0600

    pcp-816 on the UI when jobs end up erroring, it goes from 'Pending'->'Error' and now shows job output on W4.  Before it used 'Done' to show job output.

--- pcp_alpha/test/test_w4_switchto_error.py
+++ pcp_alpha/test/test_w4_switchto_error.py
@@ -4,7 +4,25 @@ from brain.connection import connect
 from rethinkdb.errors import ReqlOpFailedError
 
 
+# Function is used when testing job from Pending->Error
 def switch_to_error():
+    connection_var = connect()
+    query_obj = rtdb.db("Brain").table("Jobs").filter(
+        rtdb.row["Status"].ne("Error")).changes().run(connection_var)
+    for query_item in query_obj:
+        if query_item and query_item.get("new_val") and query_item['new_val'].get("id"):
+            print(query_item['new_val']['id'])
+            sleep(2)
+            print(rtdb.db("Brain").table("Outputs").insert(
+                {"OutputJob": query_item['new_val'],
+                 "Content": "data\noutput\n{}\n\n".format(query_item['new_val']["JobCommand"]["Inputs"][0]["Value"])}).
+                  run(connection_var))
+            print(rtdb.db("Brain").table("Jobs").get(
+                query_item['new_val']['id']).update({"Status": "Error"}).run(connection_var))
+
+
+# Function is used when testing job from Pending->Done->Error
+def switch_to_error2():
     connection_var = connect()
     query_obj = rtdb.db("Brain").table("Jobs").filter((
         rtdb.row["Status"].ne("Error") &
